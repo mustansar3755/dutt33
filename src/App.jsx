a0fab648865242b7ff1f7ab89344e809bcf2e606
mustansar3755 <mustansar3755hussain@gmail.com>
2026-01-27 20:41:05 +0500

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from '../src/components/Navbar.jsx'
-import Home from './pages/Home.jsx';
-import Services from './pages/Services.jsx';
-import Contact from './pages/Contact.jsx';
-import Footer from './components/Footer.jsx';
-import BookingModal from './components/BookingModel.jsx';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/common/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Services from "./pages/Services.jsx";
+import Contact from "./pages/Contact.jsx";
+import Footer from "./components/common/Footer.jsx";
+import BookingModal from "./components/common/BookingModel.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div>
       <div className="bg-white min-h-screen font-sans text-slate-900">
-<Navbar onOpenBooking={() => setIsModalOpen(true)} />        <Routes>
+        <Navbar onOpenBooking={() => setIsModalOpen(true)} />{" "}
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/about-us" element={<AboutUs />} />
         </Routes>
-        <Footer/>
+        <Footer />
         {/* Booking Model */}
-        <BookingModal 
-        isOpen={isModalOpen} 
-        onClose={() => setIsModalOpen(false)} 
-      />
+        <BookingModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
       </div>
     </div>
   );
---
+++ src/assets/assets.js
@@ -0,0 +1,5 @@
+import Google_Logo from './Google.png'
+
+export default{
+    Google_Logo
+}
